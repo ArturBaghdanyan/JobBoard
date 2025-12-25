98953cbd1709e03e859d83b091c9d4c4c89d15ac
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-12-26 01:32:08 +0400

    Added search container for jobs

--- src/pages/Jobs/JobsPage.tsx
+++ src/pages/Jobs/JobsPage.tsx
@@ -1,9 +1,14 @@
 
+import style from "./style.module.scss";
+
 const JobsPage = () => {
   return (
-    <div>
-        Job Page
-    </div>
+    <main>
+        <div className={style.jobs}>
+            <input type="text" placeholder="Search by title, position"/>
+            <button>Search</button>
+        </div>
+    </main>
   )
 }
 
