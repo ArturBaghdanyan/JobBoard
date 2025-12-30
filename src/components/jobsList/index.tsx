bf692056c3bf80a3cc13920aa483178ef82f0628
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-12-30 15:44:15 +0400

    Added save job logic

--- src/components/jobsList/index.tsx
+++ src/components/jobsList/index.tsx
@@ -1,5 +1,8 @@
 import type { Job } from "../../types/jobTypes";
+import { AiFillHeart } from "react-icons/ai";
+
 import style from "./style.module.scss";
+import { useState } from "react";
 
 interface JobListProps {
   jobs: Job[];
@@ -7,28 +10,50 @@ interface JobListProps {
 }
 
 export const JobList = ({ jobs, onApply }: JobListProps) => {
+  const [savedJobsId, setSavedJobIds] = useState<string[]>([]);
+
+  const toggleSave = (id: string) => {
+    setSavedJobIds((prev) =>
+      prev.includes(id) ? prev.filter((jobId) => jobId !== id) : [...prev, id]
+    );
+    console.log('saved job', id)
+  };
+  
   return (
     <div className={`${style.jobs} container`}>
-      {jobs.map((job) => (
-        <div key={job.id} className={style.jobs_container}>
-          <h2>{job.title}</h2>
-          <p>
-            {job.company} | {job.location} | {job.salary}
-          </p>
-          <p>{job.position}</p>
-          <p>{job.description}</p>
+      {jobs.map((job) => {
+        const isCurrentlySaved = savedJobsId.includes(job.id);
+        return (
+          <div key={job.id} className={style.jobs_container}>
+            <h2>{job.title}</h2>
+            <p>
+              {job.company} | {job.location} | {job.salary}
+            </p>
+            <p>{job.position}</p>
+            <p>{job.description}</p>
 
-          <button
-            disabled={job.applied}
-            onClick={() => onApply(job.id)}
-            className={style.jobs_container_apply}
-          >
-            {job.applied ? "Applied" : "Apply"}
-          </button>
+            <AiFillHeart
+              className={style.jobs_container_save}
+              onClick={() => toggleSave(job.id)}
+              style={{
+                fill: isCurrentlySaved ? "red" : "white",
+                stroke: "red",
+                strokeWidth: "50px",
+                cursor: "pointer",
+              }}
+            />
+            <button
+              disabled={job.applied}
+              onClick={() => onApply(job.id)}
+              className={style.jobs_container_apply}
+            >
+              {job.applied ? "Applied" : "Apply"}
+            </button>
 
-          <button className={style.jobs_container_more}>View more</button>
-        </div>
-      ))}
+            <button className={style.jobs_container_more}>View more</button>
+          </div>
+        );
+      })}
     </div>
   );
 };
