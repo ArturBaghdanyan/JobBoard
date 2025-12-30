9c77825a472009aacc5696c80643c9d7315ca4f5
ArturBaghdanyan <arturbaghdanyan20@gmail.com>
2025-12-31 01:41:03 +0400

    Changed Profile page

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+
+import style from "./style.module.scss";
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className={style.profile}>
+      <menu className={`${style.menu}`}>
+        <div className={style.menu_pages}>
+          <div className={style.menu_item}>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                isActive ? style.active : undefined
+              }
+            >
+              My Profile
+            </NavLink>
+            <span></span>
+          </div>
 
-export default ProfilePage
+          <div className={style.menu_item}>
+            <NavLink
+              to="/reserved"
+              className={({ isActive }) =>
+                isActive ? style.active : undefined
+              }
+            >
+              Reserved Jobs
+            </NavLink>
+            <span></span>
+          </div>
+
+          <div className={style.menu_item}>
+            <NavLink
+              to="/savedJobs"
+              className={({ isActive }) =>
+                isActive ? style.active : undefined
+              }
+            >
+              Saved Jobs
+            </NavLink>
+            <span></span>
+          </div>
+        </div>
+      </menu>
+      <main></main>
+    </div>
+  );
+};
+
+export default ProfilePage;
